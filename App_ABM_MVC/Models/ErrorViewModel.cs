a9f88cca0b59b7d0c14ca265dc30066f8d42ed45
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-02-09 14:55:56 -0300

    Initial commit

---
+++ App_ABM_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace App_ABM_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
