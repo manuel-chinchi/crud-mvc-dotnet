4778616bf7f98ecd0f3a6eb490fe3b67053a0099
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-04-09 20:46:28 -0300

    Add classdiagram.cd

--- crud_mvc/Startup.cs
+++ crud_mvc/Startup.cs
@@ -56,7 +56,9 @@ namespace crud_mvc
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
