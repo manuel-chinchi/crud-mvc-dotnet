c4d769d53093ce964580bc66cd908caa929abe53
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-11-05 16:29:45 -0300

    add database local file and connection string for database

--- crud/Data/ApplicationConfiguration.cs
+++ crud/Data/ApplicationConfiguration.cs
@@ -8,14 +8,25 @@ namespace crud.Data
         // Get connectionString from appsettings.json in Net Core
         // https://stackoverflow.com/questions/64029917/net-core-3-console-not-able-to-get-connectionstring-from-appsettings
         // https://learn.microsoft.com/en-us/ef/core/miscellaneous/connection-strings
-        public static string GetConnectionString(string connectionString)
+        // Read '|DataDirectory|' in connectionString
+        // https://stackoverflow.com/questions/37058684/how-to-set-the-right-attachdbfilename-relative-path-in-asp-net-core
+        public static string GetConnectionString(string nameConnectionString)
         {
+            // Get connection string
             IConfigurationRoot configuration = new ConfigurationBuilder()
                     .SetBasePath(Path.Combine(Directory.GetCurrentDirectory()))
                     .AddJsonFile("appsettings.json", optional: false)
                     .Build();
 
-            return configuration.GetConnectionString(connectionString);
+            string connectionString = configuration.GetConnectionString(nameConnectionString);
+
+            // Read |DataDirectory|
+            if (connectionString.Contains("|DataDirectory|"))
+            {
+                connectionString = connectionString.Replace("|DataDirectory|", Directory.GetCurrentDirectory());
+            }
+
+            return connectionString;
         }
     }
 }
