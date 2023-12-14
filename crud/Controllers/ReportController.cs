5a8db39a0b907ef1982011c74f408db586be6683
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2023-12-14 16:22:10 -0300

    changes of release 1.2 - add page with info of project - add reports charts and button download - add button for export table data to CSV, Excel, PDF formats - fix: adjust responsive various sections

---
+++ crud/Controllers/ReportController.cs
@@ -0,0 +1,42 @@
+﻿using crud.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud.Controllers
+{
+    public class ReportController : BaseController
+    {
+        public IActionResult Reports()
+        {
+            ViewBag.ChartTypes = new List<ChartInfo>()
+            {
+                new ChartInfo() { Name = "Pastel", Value = "pie" },
+                new ChartInfo() { Name = "Barras", Value = "bar" }
+            };
+
+            ViewBag.Title = "Reportes";
+            ViewBag.Message = "Lista de reportes";
+
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetDataCategories()
+        {
+            List<object> dataCategories = categoryService.
+                GetCategories().
+                Select(c => new
+                {
+                    name = c.Name,
+                    quantity = c.Articles.Count
+                }).
+                Cast<object>().
+                ToList();
+
+            return Json(dataCategories);
+        }
+    }
+}
--- crud/wwwroot/js/datatables-custom.js
+++ crud/wwwroot/js/datatables-custom.js
@@ -1,19 +1,62 @@
 ﻿// Documentation 
 // https://datatables.net/reference/option/language
+// https://datatables.net/extensions/buttons/examples/html5/columns.html
 
 $(document).ready(function () {
-    $('table').DataTable({
+    tableRef.DataTable({
         language: {
             info: 'Mostrando registro _START_ hasta el _END_',
             infoEmpty: '',
             infoFiltered: '',
             zeroRecords: 'No se encontraron registros coincidentes',
-            search: 'Buscar:',
+            search: '',
+            searchPlaceholder: "Buscar",
             lengthMenu: 'Mostrar _MENU_ registros',
             paginate: {
                 previous: 'Anterior',
                 next: 'Siguiente'
+            },
+            buttons: {
+                copyTitle: 'Copiado al portapapeles',
+                copySuccess: {
+                    _: '%d líneas copiadas',
+                },
             }
         },
+        dom: 'lBfrtip',
+        buttons: [
+            {
+                extend: 'collection',
+                background: false,
+                text: 'Exportar',
+                buttons: [
+                    {
+                        extend: 'copy',
+                        text: 'Copiar',
+                        exportOptions: {
+                            columns: tableCols
+                        }
+                    },
+                    {
+                        extend: 'csv',
+                        exportOptions: {
+                            columns: tableCols
+                        }
+                    },
+                    {
+                        extend: 'excel',
+                        exportOptions: {
+                            columns: tableCols
+                        }
+                    },
+                    {
+                        extend: 'pdf',
+                        exportOptions: {
+                            columns: tableCols
+                        },
+                    },
+                ]
+            },
+        ]
     });
 });
